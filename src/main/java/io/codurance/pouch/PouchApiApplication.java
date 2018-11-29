c618aa0f4844ba7a1b47dc9805dc7077a0e99710
SirXOPHER <chris.reinmuller@gmail.com>
2018-11-29 11:39:42 +0000

    Add basic project directory structure

---
+++ src/main/java/io/codurance/pouch/PouchApiApplication.java
@@ -0,0 +1,12 @@
+package io.codurance.pouch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PouchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PouchApiApplication.class, args);
+	}
+}
