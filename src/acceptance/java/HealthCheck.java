d6f1d03a1d55ec347b57cc8c49ed9dfd54c05b1e
SirXOPHER <chris.reinmuller@gmail.com>
2018-11-30 18:44:15 +0000

    Add preliminary health check provided by Spring Boot Actuator

--- src/acceptance/java/HealthCheck.java
+++ src/acceptance/java/HealthCheck.java
@@ -1,20 +1,27 @@
 import io.codurance.pouch.PouchApiApplication;
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
 
 import static io.restassured.RestAssured.when;
 import static org.hamcrest.Matchers.equalTo;
 
-@SpringBootTest(classes= PouchApiApplication.class)
+@SpringBootTest(classes = PouchApiApplication.class, webEnvironment = WebEnvironment.RANDOM_PORT)
 class HealthCheck {
+
+    @LocalServerPort
+    private int serverPort;
 
     @Test
     void returns_200_with_expected_health_check_token() {
 
-        when().
-                get("/healthcheck").
-        then().
-                statusCode(200).
-                body("health.check", equalTo("alive"));
+        RestAssured.port = serverPort;
+
+        when().get("/health")
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("UP"));
     }
 }
