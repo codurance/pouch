21cc2d71e608cbe6a40a32ae44b3a944f868c78e
SirXOPHER <chris.reinmuller@gmail.com>
2018-12-05 16:11:47 +0000

    Add database migration script to insert test data into table resources

---
+++ src/main/resources/db/migration/V2__INSERT_INTO_RESOURCES.sql
@@ -0,0 +1,5 @@
+INSERT INTO RESOURCES (ID, ADDED_ON, TITLE, URL) VALUES (
+    1, '2018-12-05 16:01:00', 'Spring Data JDBC', 'https://spring.io/projects/spring-data-jdbc'
+    2, '2018-12-05 16:02:00', 'SQL Fiddle', 'http://sqlfiddle.com/'
+    3, '2018-12-05 16:03:00', 'PostgreSQL: The world''s most advanced open source database', 'https://www.postgresql.org/'
+);
