0e5639fa082b498e8156b2247e52d46e7c7e2000
SirXOPHER <chris.reinmuller@gmail.com>
2018-12-07 18:36:11 +0000

    Enable auto-configuration of the Spring Application Context

--- src/main/java/io/codurance/pouch/PouchApiApplication.java
+++ src/main/java/io/codurance/pouch/PouchApiApplication.java
@@ -1,9 +1,11 @@
 package io.codurance.pouch;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class PouchApiApplication {
 
 	public static void main(String[] args) {
