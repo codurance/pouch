b16f69333bc5d6d11f4625014f8c0d59e6b2e694
SirXOPHER <chris.reinmuller@gmail.com>
2018-12-07 18:45:33 +0000

    Configure SpringBootTest annotation on default unit test

--- src/test/java/io/codurance/pouch/PouchApiApplicationTests.java
+++ src/test/java/io/codurance/pouch/PouchApiApplicationTests.java
@@ -3,10 +3,11 @@ package io.codurance.pouch;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = PouchApiApplication.class, webEnvironment = WebEnvironment.RANDOM_PORT)
 public class PouchApiApplicationTests {
 
 	@Test
