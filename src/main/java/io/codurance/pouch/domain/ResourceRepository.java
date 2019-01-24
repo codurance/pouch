2dcf965fca78888f9f16d578ed98ec982096bd3b
SirXOPHER <chris.reinmuller@gmail.com>
2019-01-24 23:51:15 +0000

    Provide custom query on CRUD Repository

--- src/main/java/io/codurance/pouch/domain/ResourceRepository.java
+++ src/main/java/io/codurance/pouch/domain/ResourceRepository.java
@@ -1,8 +1,15 @@
 package io.codurance.pouch.domain;
 
 import io.codurance.pouch.domain.Resource;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
-public interface ResourceRepository extends CrudRepository<Resource, UUID> {}
+public interface ResourceRepository extends CrudRepository<Resource, UUID> {
+
+    @Query("SELECT r.id, r.added, r.title, r.url, r.favourite FROM Resource r WHERE r.title=:title")
+    List<Resource> findByTitle(@Param("title") String title);
+}
